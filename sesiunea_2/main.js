bd11e14d5178b2b1b779d8c54dd6e76f50cdd008
Igna Ovidiu <ovidiu.igna92@yahoo.com>
2022-08-22 13:50:33 +0300

    Switch

--- sesiunea_2/main.js
+++ sesiunea_2/main.js
@@ -8,3 +8,20 @@ while (condition) {
     condition = false;
     }
 }
+
+const money = 1250;
+
+switch (money) {
+    case 1250:
+        console.log('Ai primit marire');
+        break;
+    case 900:
+        console.log('Ti-am taiat din salariu');
+        break;
+    case 2000:
+        console.log('S-a produs o greseala')
+        break;
+    default:
+        console.log('Au Intrat banii')
+        break;
+}
