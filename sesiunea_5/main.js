7470785ad56c8660233a7f0a618b2a5a2880d2a2
Igna Ovidiu <ovidiu.igna92@yahoo.com>
2022-08-31 21:49:57 +0300

    User Data

---
+++ sesiunea_5/main.js
@@ -0,0 +1,50 @@
+
+
+
+const userFirstName = document.querySelector("#firstName")
+const userLastName = document.querySelector("#lastName")
+const userDateOfBirth = document.querySelector("#dateOfBirth")
+const userHeight = document.querySelector("#height")
+const userWeight = document.querySelector("#weight")
+const userEmail = document.querySelector("#email")
+const userPhoneNumber = document.querySelector("#phoneNumber")
+const userSubmit = document.querySelector("#submitForm")
+const messageElement = document.getElementById('message')
+
+
+
+userSubmit.addEventListener('click', (e) => {
+    submitData()
+})
+
+
+class User {
+    constructor(firstName, lastName, dateOfBirth, height, weight, email, phoneNumber) {
+        this.name = `${firstName} ${lastName}`;
+        this.dateOfBirth = dateOfBirth;
+        this.height = height;
+        this.weight = weight;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.userCreated = false; 
+    }
+
+    createUser() {
+        console.log(`User Was Created Successfully 👤`);
+    }
+}
+
+
+function submitData() {
+    const Users = new User(userFirstName.value, userLastName.value, userDateOfBirth.value, userHeight.value, userWeight.value, userEmail.value, userPhoneNumber.value)
+    console.log(Users)
+
+    const userlist = document.querySelector(".userlist")
+
+    userlist.innerHTML = `<li>${userFirstName.value} ${userLastName.value} ${userDateOfBirth.value} ${userEmail.value} ${userPhoneNumber.value}</li>`
+}
+
+function createUser () {
+    console.log(`User Was Created Successfully 👤`)
+    this.userCreated = true;
+}
